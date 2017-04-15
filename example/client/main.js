bdf1ecf4c175cf75f8f1433217b03947d102247f
StepanLylak <AnimeWars23>
2017-04-15 15:25:12 +0200

    remove example source

--- example/client/main.js
+++
@@ -1,9 +0,0 @@
-import { Template } from 'meteor/templating';
-import ModalBoxes from 'meteor/lylak:modal-boxes';
-
-import '../imports/startup/client/modal-boxes-configuration.js';
-
-import '../imports/ui/components/hello/hello.js';
-import '../imports/ui/components/modal-boxes/modal-one/modal-one.js';
-import '../imports/ui/components/modal-boxes/modal-two/modal-two.js';
-import '../imports/ui/components/modal-boxes/modal-three/modal-three.js';
